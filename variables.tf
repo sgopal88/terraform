d83e645802ef8409c6f4579cbb48dfde15395682
Srinivasan Gopalakrishnan <srinivasan.gopalakrishnan@hitachids.com>
2025-09-04 23:23:40 -0500

    Environment path typo fix 9

--- variables.tf
+++ variables.tf
@@ -1,7 +1,17 @@
-variable "project"              { type = string }
-variable "env"                  { type = string }
-variable "cidr_block"           { type = string }
-variable "public_subnets_cidrs" { type = list(string) }
-variable "private_subnets_cidrs"{ type = list(string) }
-variable "availability_zones"   { type = list(string) }
-variable "aws_region" { type = list(string) }
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (dev/stage/prod)"
+  type        = string
+}
+
+
